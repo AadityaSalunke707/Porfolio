664e73635b14db1d0b077491c783253fa754f29f
AadityaSalunke707 <adityasalunke707@gmail.com>
2024-12-08 15:17:49 +0530

    saved!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ import { logo } from "../assets/images";
 const Navbar = () => {
   return (
     <header className='header'>
-      <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+      <NavLink to='/' 
+      className="w-10 h-10 rounde-lg bg-white flex items-center justify-center font-bold shadow-md">
+      {/*) <img src={logo} alt='logo' className='w-18 h-18 object-contain' />*/}
+      <p className="blue-gradient_text">AS</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-400" : "text-black" }>
